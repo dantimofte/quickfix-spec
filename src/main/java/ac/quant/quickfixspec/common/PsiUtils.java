7cf50cd164052503c7af3662648d31f3e5d1d13c
Dan Timofte <dantimofte@proton.me>
2024-09-28 22:20:13 +0300

    Add documentation on mouse hover

--- src/main/java/ac/quant/quickfixspec/common/PsiUtils.java
+++ src/main/java/ac/quant/quickfixspec/common/PsiUtils.java
@@ -2,6 +2,8 @@ package ac.quant.quickfixspec.common;
 
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.xml.XmlAttribute;
+import com.intellij.psi.xml.XmlAttributeValue;
 import com.intellij.psi.xml.XmlTag;
 import org.jetbrains.annotations.Nullable;
 
@@ -20,4 +22,25 @@ public class PsiUtils {
         return null;
     }
 
+    public static @Nullable XmlTag findComponent(final XmlTag rootTag, final String componentName) {
+
+        XmlTag[] componentsTags = rootTag.findSubTags("components");
+
+        for (XmlTag componentsTag : componentsTags) {
+            for (XmlTag componentTag : componentsTag.findSubTags("component")) {
+                String nameAttr = componentTag.getAttributeValue("name");
+                if (componentName.equals(nameAttr)) {
+                    return componentTag;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static boolean isComponentsDeclaration(XmlAttributeValue valueElement) {
+        XmlAttribute parentAttribute = (XmlAttribute) valueElement.getParent();
+        XmlTag xmlTag = parentAttribute.getParent();
+        XmlTag parentTag = xmlTag.getParentTag();
+        return parentTag != null && "components".equals(parentTag.getName());
+    }
 }
---
+++ src/main/kotlin/ac/quant/quickfixspec/documentation/QuickfixComponentDocumentationTargetProvider.kt
@@ -0,0 +1,49 @@
+package ac.quant.quickfixspec.documentation
+
+//class QuickfixComponentDocumentationTargetProvider : DocumentationTargetProvider {
+
+import ac.quant.quickfixspec.common.PsiUtils.getRootTag
+import com.intellij.platform.backend.documentation.DocumentationTarget
+import com.intellij.platform.backend.documentation.DocumentationTargetProvider
+import com.intellij.psi.PsiFile
+import com.intellij.psi.xml.XmlAttribute
+import com.intellij.psi.xml.XmlTag
+import com.intellij.psi.xml.XmlAttributeValue
+
+class QuickfixComponentDocumentationTargetProvider : DocumentationTargetProvider {
+
+    var rootTag: XmlTag? = null
+
+    override fun documentationTargets(file: PsiFile, offset: Int): List<DocumentationTarget> {
+
+        val element = file.findElementAt(offset) ?: return emptyList()
+        val attributeValue = element.parent as? XmlAttributeValue ?: return emptyList()
+        val attribute = attributeValue.parent as? XmlAttribute ?: return emptyList()
+
+        if (rootTag == null) {
+            rootTag = getRootTag(attribute)
+        }
+
+        if ("name" != attribute.name) {
+            return emptyList()
+        }
+
+        val tag = attribute.parent as? XmlTag ?: return emptyList()
+
+        if ("component" != tag.name) {
+            return emptyList()
+        }
+
+        val tagParent = tag.parent as? XmlTag ?: return emptyList()
+
+        if ("components" == tagParent.name) {
+            return emptyList()
+        }
+
+        val componentName = attribute.value ?: return emptyList()
+        return listOf(QuickfixComponentDocumentationTarget(componentName, rootTag))
+
+    }
+
+
+}
