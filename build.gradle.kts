1e91991b82582f695b34eef989332f887cd813bf
Dan Timofte <dantimofte@proton.me>
2024-11-12 10:40:44 +0200

    bump junit version

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,8 +37,8 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok:1.18.34")
     testImplementation(libs.junit)
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.3")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.11.3")
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
